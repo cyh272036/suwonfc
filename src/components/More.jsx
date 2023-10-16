a1bb92e8b7dddc1378e0f11bfcee45f3043a42e7
cyh272036 <86253660+cyh272036@users.noreply.github.com>
2023-10-16 14:37:00 +0900

    2023/10/16-middle/final

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -24,7 +24,7 @@ const More = (props) => {
     return (
 
         <Col md={6} lg={4}>
-            <img src={process.env.PUBLIC_UR + imgUrl} width="100%" alt='moreImg' style={{ border: '1px solid #eee' }} />
+            <img src={process.env.PUBLIC_URL + imgUrl} width="100%" alt='moreImg' style={{ border: '1px solid #eee' }} />
             <h4 style={text_one}>{title}</h4>
             <p style={text_two}>{price}원</p>
         </Col>
